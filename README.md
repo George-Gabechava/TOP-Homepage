368f7a8ddaf127d97bf9611666e51b82c5dbfd0e
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2024-03-05 10:05:13 -0500

    added 2 project cards

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TOP-Homepage
+
+Image by <a href="https://pixabay.com/users/anncarter-162688/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1568623">Ann Carter</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1568623">Pixabay</a>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,53 @@
 console.log("hi");
 
+
+const gitButton = document.getElementById('gitButtonMain');
+gitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.location.href = 'https://github.com/George-Gabechava';
+});
+
+const linkedInButton = document.getElementById('linkedInButtonMain');
+linkedInButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.location.href = 'https://www.linkedin.com/in/george-gabechava-92905b85';
+});
+
+const project1pic = document.getElementsByClassName('link1')[0];
+project1pic.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://george-gabechava.github.io/TOP-Battleship/', '_blank');
+});
+
+const project1 = document.getElementsByClassName('link1')[1];
+project1.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://george-gabechava.github.io/TOP-Battleship/', '_blank');
+});
+
+const project1git = document.getElementById("git1");
+project1git.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://github.com/George-Gabechava/TOP-Battleship', '_blank');
+});
+
+const project2pic = document.getElementsByClassName('link2')[0];
+project2pic.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://george-gabechava.github.io/TOP-Weather-API/', '_blank');
+});
+
+const project2 = document.getElementsByClassName('link2')[1];
+project2.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://george-gabechava.github.io/TOP-Weather-API/', '_blank');
+});
+
+const project2git = document.getElementById("git2");
+project2git.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://github.com/George-Gabechava/TOP-Weather-API', '_blank');
+});
+
+
 // module.exports = { };
